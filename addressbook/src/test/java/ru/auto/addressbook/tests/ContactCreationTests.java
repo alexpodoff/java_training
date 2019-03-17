6e9a56a479b39de17d9f216fe986c68a2e9b2c07
Alexandr Struchkov <alexpodoff@gmail.com>
2019-03-17 15:51:04 +0300

    Helpers added, ContnactCreationTests refactored

--- addressbook/src/test/java/ru/auto/addressbook/tests/ContactCreationTests.java
+++ addressbook/src/test/java/ru/auto/addressbook/tests/ContactCreationTests.java
@@ -1,77 +1,17 @@
 package ru.auto.addressbook.tests;
 
-import java.util.concurrent.TimeUnit;
-import org.testng.annotations.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
 import ru.auto.addressbook.model.ContactData;
 
-public class ContactCreationTests {
-  private WebDriver wd;
-
-  @BeforeClass(alwaysRun = true)
-  public void setUp() {
-    wd = new FirefoxDriver();
-    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/");
-    login("admin", "secret");
-  }
-
-  private void login(String username, String password) {
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
-  }
+public class ContactCreationTests extends TestBase {
 
   @Test
   public void testContactCreation() {
-    gotoHomePage();
-    initContactCreation();
-    fillContactForm(new ContactData("test1", "test2", "aaa bbb ccc", "12345678", "omg@my.dom"));
-    submitContactCreation();
-    gotoHomePage();
+    app.getNavigationHelper().gotoHomePage();
+    app.getContactHelper().initContactCreation();
+    app.getContactHelper().fillContactForm(new ContactData("test1", "test2", "aaa bbb ccc", "12345678", "omg@my.dom"));
+    app.getContactHelper().submitContactCreation();
+    app.getNavigationHelper().gotoHomePage();
   }
 
-  private void submitContactCreation() {
-    wd.findElement(By.name("submit")).click();
-  }
-
-  private void fillContactForm(ContactData contactData) {
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
-    wd.findElement(By.name("address")).click();
-    wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
-    wd.findElement(By.name("home")).click();
-    wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys(contactData.getHomephone());
-    wd.findElement(By.name("email")).click();
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
-  }
-
-  private void gotoHomePage() {
-    wd.findElement(By.linkText("home")).click();
-  }
-
-  private void initContactCreation() {
-    wd.findElement(By.linkText("add new")).click();
-  }
-
-  private void logout() {
-    wd.findElement(By.linkText("Logout")).click();
-  }
-
-  @AfterClass(alwaysRun = true)
-  public void tearDown() {
-    logout();
-    wd.quit();
-  }
 }
