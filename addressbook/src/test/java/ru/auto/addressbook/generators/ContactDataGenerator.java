becb1dee5b4d3fde97c9084a39e52d8b79ffd87c
Alexandr Struchkov <alexpodoff@gmail.com>
2019-04-08 10:18:31 +0400

    data generators added

--- addressbook/src/test/java/ru/auto/addressbook/generators/ContactDataGenerator.java
+++ addressbook/src/test/java/ru/auto/addressbook/generators/ContactDataGenerator.java
@@ -6,7 +6,7 @@ import com.beust.jcommander.ParameterException;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.thoughtworks.xstream.XStream;
-import ru.auto.addressbook.model.GroupData;
+import ru.auto.addressbook.model.ContactData;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -15,7 +15,7 @@ import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GroupDataGenerator {
+public class ContactDataGenerator {
 
     @Parameter(names = "-c", description = "Group count")
     private int count;
@@ -27,7 +27,7 @@ public class GroupDataGenerator {
     private String format;
 
     public static void main(String[] args) throws IOException {
-        GroupDataGenerator generator = new GroupDataGenerator();
+        ContactDataGenerator generator = new ContactDataGenerator();
         JCommander jCommander = new JCommander(generator);
         try {
             jCommander.parse(args);
@@ -40,16 +40,16 @@ public class GroupDataGenerator {
     }
 
     private void run() throws IOException {
-        List<GroupData> groups = generateGroups(count);
+        List<ContactData> contacts = generateContacts(count);
         switch (format) {
             case "csv":
-                saveAsCsv(groups, new File(file));
+                saveAsCsv(contacts, new File(file));
                 break;
             case "xml":
-                saveAsXml(groups, new File(file));
+                saveAsXml(contacts, new File(file));
                 break;
             case "json":
-                saveAsJson(groups, new File(file));
+                saveAsJson(contacts, new File(file));
                 break;
             default:
                 System.out.println("Unrecognized format" + format);
@@ -58,39 +58,44 @@ public class GroupDataGenerator {
 
     }
 
-    private void saveAsJson(List<GroupData> groups, File file) throws IOException {
+    private void saveAsJson(List<ContactData> contacts, File file) throws IOException {
         Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
-        String json = gson.toJson(groups);
+        String json = gson.toJson(contacts);
         Writer writer = new FileWriter(file);
         writer.write(json);
         writer.close();
     }
 
-    private void saveAsXml(List<GroupData> groups, File file) throws IOException {
+    private void saveAsXml(List<ContactData> contacts, File file) throws IOException {
         XStream xstream = new XStream();
-        xstream.processAnnotations(GroupData.class);
-        String xml = xstream.toXML(groups);
+        xstream.processAnnotations(ContactData.class);
+        String xml = xstream.toXML(contacts);
         Writer writer = new FileWriter(file);
         writer.write(xml);
         writer.close();
     }
 
 
-    private void saveAsCsv(List<GroupData> groups, File file) throws IOException {
+    private void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
         Writer writer = new FileWriter(file);
-        for (GroupData group: groups) {
-            writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
+        for (ContactData contact: contacts) {
+            writer.write(String.format("%s;%s;%s;%s\n",
+                    contact.getFirstname(),
+                    contact.getLastname(),
+                    contact.getAddress(),
+                    contact.getHomephone()));
         }
         writer.close();
     }
 
-    private List<GroupData> generateGroups(int count) {
-        List<GroupData> groups = new ArrayList<>();
+    private List<ContactData> generateContacts(int count) {
+        List<ContactData> contacts = new ArrayList<>();
         for (int i = 0; i < count; i++) {
-            groups.add(new GroupData().withName(String.format("test %s", i))
-                    .withHeader(String.format("header %s", i))
-                    .withFooter(String.format("footer %s", i)));
+            contacts.add(new ContactData().withFirstname(String.format("test %s", i))
+                    .withLastname(String.format("lastname %s", i))
+                    .withAddress("some test address")
+                    .withHomephone("12345678"));
         }
-        return groups;
+        return contacts;
     }
 }
--- addressbook/src/test/java/ru/auto/addressbook/tests/GroupCreationTests.java
+++ addressbook/src/test/java/ru/auto/addressbook/tests/GroupCreationTests.java
@@ -1,24 +1,61 @@
 package ru.auto.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.auto.addressbook.model.GroupData;
 import ru.auto.addressbook.model.Groups;
+
+import java.io.*;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-  @Test
-  public void testGroupCreation() {
-    app.goTO().GroupPage();
-    Groups before = app.group().all();
-    GroupData group = new GroupData().withName("test111");
-    app.group().create(group);
-    assertThat(app.group().count(), equalTo(before.size() + 1));
-    Groups after = app.group().all();
-    assertThat(after, equalTo(
-            before.withAdded(group.withId(after.stream().mapToInt(GroupData::getId).max().getAsInt()))));
-  }
+    @DataProvider
+    public Iterator<Object[]> validGroupsFromXml() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
+        String xml = "";
+        String line = reader.readLine();
+        while (line != null) {
+            xml += line;
+            line = reader.readLine();
+        }
+        XStream xstream = new XStream();
+        xstream.processAnnotations(GroupData.class);
+        List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
+        return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+    }
+
+    @DataProvider
+    public Iterator<Object[]> validGroupsFromJson() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.json")));
+        String json = "";
+        String line = reader.readLine();
+        while (line != null) {
+            json += line;
+            line = reader.readLine();
+        }
+        Gson gson = new Gson();
+        List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>() {}.getType());
+        return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+    }
+
+    @Test(dataProvider = "validGroupsFromJson")
+    public void testGroupCreation(GroupData group) {
+        app.goTO().GroupPage();
+        Groups before = app.group().all();
+        app.group().create(group);
+        assertThat(app.group().count(), equalTo(before.size() + 1));
+        Groups after = app.group().all();
+        assertThat(after, equalTo(
+                before.withAdded(group.withId(after.stream().mapToInt(GroupData::getId).max().getAsInt()))));
+    }
 
 }
