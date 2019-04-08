7162b15efc0978190a00075476ab8c9ffa16c738
Alexandr Struchkov <alexpodoff@gmail.com>
2019-04-08 12:15:00 +0400

    logger added

--- addressbook/src/test/java/ru/auto/addressbook/tests/TestBase.java
+++ addressbook/src/test/java/ru/auto/addressbook/tests/TestBase.java
@@ -1,13 +1,18 @@
 package ru.auto.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.*;
 import ru.auto.addressbook.appmanager.ApplicationManager;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class TestBase {
+
+    private Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     protected static final ApplicationManager app
             = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
@@ -22,4 +27,13 @@ public class TestBase {
         app.stop();
     }
 
+    @BeforeMethod
+    public void logTestStart(Method m, Object[] p) {
+        logger.info(String.format("Start test %s with params %s", m.getName(), Arrays.asList(p)));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void logTestStop(Method m) {
+        logger.info(String.format("Stop test %s", m.getName()));
+    }
 }
