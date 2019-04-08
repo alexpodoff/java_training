eca9f5c0aa2ec023f647cbe5f930d726902c1f99
Alexandr Struchkov <alexpodoff@gmail.com>
2019-04-08 10:25:51 +0400

    test refactored
    getting data from file in different formats

--- addressbook/src/test/java/ru/auto/addressbook/tests/ContactCreationTests.java
+++ addressbook/src/test/java/ru/auto/addressbook/tests/ContactCreationTests.java
@@ -1,24 +1,59 @@
 package ru.auto.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.auto.addressbook.model.ContactData;
 import ru.auto.addressbook.model.Contacts;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 public class ContactCreationTests extends TestBase {
 
-  @Test
-  public void testContactCreation() {
+  @DataProvider
+  public Iterator<Object[]> validContactsFromXml() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+    String xml = "";
+    String line = reader.readLine();
+    while (line != null) {
+      xml += line;
+      line = reader.readLine();
+    }
+    XStream xstream = new XStream();
+    xstream.processAnnotations(ContactData.class);
+    List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+    return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+  }
+
+  @DataProvider
+  public Iterator<Object[]> validContactsFromJson() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null) {
+      json += line;
+      line = reader.readLine();
+    }
+    Gson gson = new Gson();
+    List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>() {}.getType());
+    return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+  }
+
+  @Test(dataProvider = "validContactsFromJson")
+  public void testContactCreation(ContactData contact) {
     app.goTO().homePage();
     Contacts before = app.contact().all();
-    ContactData contact = new ContactData()
-            .withFirstname("test1")
-            .withLastname("test2")
-            .withAddress("aaa bbb ccc")
-            .withHomephone("12345678")
-            .withEmail("omg@my.dom");
     app.contact().create(contact);
     app.goTO().homePage();
     assertThat(app.contact().count(), equalTo(before.size() + 1));
