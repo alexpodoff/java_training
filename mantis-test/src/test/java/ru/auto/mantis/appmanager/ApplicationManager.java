18a3893c77d4670a03b07aae6ddef3f9a47dbc50
Alexandr Struchkov <alexpodoff@gmail.com>
2019-04-24 18:52:18 +0300

    lazy init and RegistrationHelper added

--- mantis-test/src/test/java/ru/auto/mantis/appmanager/ApplicationManager.java
+++ mantis-test/src/test/java/ru/auto/mantis/appmanager/ApplicationManager.java
@@ -16,7 +16,8 @@ import java.util.concurrent.TimeUnit;
 public class ApplicationManager {
 
     private final Properties properties;
-    WebDriver wd;
+    private RegistrationHelper registrationHelper;
+    private WebDriver wd;
     private String browser;
 
     public ApplicationManager(String browser) {
@@ -27,25 +28,31 @@ public class ApplicationManager {
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-
-        switch (browser) {
-            case BrowserType.FIREFOX:
-                wd = new FirefoxDriver();
-                break;
-            case BrowserType.CHROME:
-                wd = new ChromeDriver();
-                break;
-            case BrowserType.IE:
-                wd = new InternetExplorerDriver();
-                break;
-        }
-        wd.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
-        wd.get(properties.getProperty("web.baseUrl"));
     }
 
+    public WebDriver getDriver() {
+        if (wd == null) {
+            switch (browser) {
+                case BrowserType.FIREFOX:
+                    wd = new FirefoxDriver();
+                    break;
+                case BrowserType.CHROME:
+                    wd = new ChromeDriver();
+                    break;
+                case BrowserType.IE:
+                    wd = new InternetExplorerDriver();
+                    break;
+            }
+            wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+            wd.get(properties.getProperty("web.baseUrl"));
+        }
+        return wd;
+    }
 
     public void stop() {
-        wd.quit();
+        if (wd != null) {
+            wd.quit();
+        }
     }
 
     public String getProperty(String key) {
@@ -55,4 +62,12 @@ public class ApplicationManager {
     public HttpSession newSession() {
         return new HttpSession(this);
     }
+
+    public RegistrationHelper registration() {
+        if (registrationHelper == null) {
+            registrationHelper = new RegistrationHelper(this);
+        }
+        return registrationHelper;
+    }
+
 }
